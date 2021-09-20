a6fc68a97bcb63de11c6f2c7f115da5e4c09a8dc
Scotet <mscotet.lmit@gmail.com>
2021-09-20 13:47:39 +0200

    CoroutineUtils -> parrallel

--- core/src/commonMain/kotlin/tech/skot/core/coroutinesUtils.kt
+++ core/src/commonMain/kotlin/tech/skot/core/coroutinesUtils.kt
@@ -5,6 +5,16 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 
 suspend fun <R> atomic(block: suspend CoroutineScope.() -> R): R =
-        GlobalScope.async(block = block)
-                .await()
+    GlobalScope.async(block = block)
+        .await()
 
+
+suspend fun parrallel(coroutineScope: CoroutineScope, vararg blocks: suspend () -> Unit) {
+    val deffereds =
+        blocks.map {
+            coroutineScope.async {
+                it()
+            }
+        }
+    deffereds.forEach { it.await() }
+}

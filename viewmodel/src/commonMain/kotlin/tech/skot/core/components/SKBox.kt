5e306cfb0ce3d9dc2a788f5076a037839640b130
Scotet <mscotet.lmit@gmail.com>
2021-12-09 18:41:32 +0100

    [FIX] SKBox, initialisation de la propriété "items" du VM
    la propriété n'était pas initialisée en cas de constructeur avec des items initiaux

--- viewmodel/src/commonMain/kotlin/tech/skot/core/components/SKBox.kt
+++ viewmodel/src/commonMain/kotlin/tech/skot/core/components/SKBox.kt
@@ -8,7 +8,7 @@ open class SKBox(items: List<SKComponent<*>>?) : SKComponent<SKBoxVC>() {
 
     override val view = coreViewInjector.skBox(items?.map { it.view } ?: emptyList(), hiddenInitial = null)
 
-    var items: List<SKComponent<*>> = emptyList()
+    var items: List<SKComponent<*>> = items ?: emptyList()
         set(value) {
             view.items = value.map { it.view }
             field.forEach { if (!value.contains(it)) it.onRemove() }

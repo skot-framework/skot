717c778d373b0f0f1736e20f7a30918f2c35d895
Mathieu Scotet <mscotet.lmit@gmail.com>
2022-01-26 16:52:12 +0100

    AGP 7.1.0
    Attention -> passer gradle en 7.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -15,7 +15,7 @@ object Versions {
 
     object Android {
 
-        const val gradle = "7.0.4"
+        const val gradle = "7.1.0"
 
         const val minSdk = 21
         const val compileSdk = 31

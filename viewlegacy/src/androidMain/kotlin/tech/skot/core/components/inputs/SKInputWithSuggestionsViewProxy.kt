f935b7cdffdc9bc19ba79781fd3a6ef7ac8d92de
Mathieu Scotet <mscotet.lmit@gmail.com>
2022-01-26 16:44:48 +0100

    SKInputWithSuggestion
    possibilité d'être utilisé dans une SKBox

--- viewlegacy/src/androidMain/kotlin/tech/skot/core/components/inputs/SKInputWithSuggestionsViewProxy.kt
+++ viewlegacy/src/androidMain/kotlin/tech/skot/core/components/inputs/SKInputWithSuggestionsViewProxy.kt
@@ -1,10 +1,14 @@
 package tech.skot.core.components.inputs
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import tech.skot.core.SKLog
 import tech.skot.core.components.SKActivity
 import tech.skot.view.live.SKMessage
 import tech.skot.viewlegacy.R
+import tech.skot.viewlegacy.databinding.SkAlertInputBinding
 import tech.skot.viewlegacy.databinding.SkInputWithSuggestionsBinding
 
 
@@ -31,7 +35,15 @@ class SKInputWithSuggestionsViewProxy(
     SKInputWithSuggestionsVC {
 
     override val layoutId: Int = R.layout.sk_input_with_suggestions
-    override fun bindingOf(view: View) = SkInputWithSuggestionsBinding.bind(view)
+    override fun bindingOf(view: View): SkInputWithSuggestionsBinding {
+        return SkInputWithSuggestionsBinding.bind(view)
+    }
+
+    override fun inflate(
+        layoutInflater: LayoutInflater,
+        parent: ViewGroup?,
+        attachToParent: Boolean
+    ): SkInputWithSuggestionsBinding = SkInputWithSuggestionsBinding.inflate(layoutInflater, parent, attachToParent)
 
     private val requestFocusMessage = SKMessage<Unit>()
     override fun requestFocus() {

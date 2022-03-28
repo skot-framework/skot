f9a5a629570937c748a144779f94b30e7499d7e5
Sylvain Guéniot <s.gueniot@useradgents.com>
2022-03-28 09:30:56 +0200

    [FEAT] simplify image button usage

--- viewmodel/src/commonMain/kotlin/tech/skot/core/components/inputs/SKImageButton.kt
+++ viewmodel/src/commonMain/kotlin/tech/skot/core/components/inputs/SKImageButton.kt
@@ -4,6 +4,15 @@ import tech.skot.core.components.SKComponent
 import tech.skot.core.di.coreViewInjector
 import tech.skot.core.view.Icon
 
+/**
+ * SKImageButton is as [SKComponent] that could be used to show an ImageButton
+ * @param icon the initial [Icon] of the ImageButton
+ * @param enabled the initial [enable][Boolean] state of the imageButton
+ * @param hidden the initial [visibility][Boolean] state of the imageButton
+ * @param debounce the [period][Long] during which second tap is not taken into account
+ * @param onTap the initial on tap callback lambda
+ */
+@Suppress("unused")
 open class SKImageButton(
     icon: Icon,
     enabled:Boolean? = null,
@@ -20,5 +29,40 @@ open class SKImageButton(
         debounce = debounce
     )
 
+    /**
+     *  use it to set/get the [enable][Boolean] state of the imageButton
+     */
+    var enabled: Boolean?
+        get() = view.enabled
+        set(value) {
+            view.enabled = value
+        }
+
+    /**
+     *  use it to set/get the [visibility][Boolean] state of the imageButton
+     */
+    var hidden: Boolean?
+        get() = view.hidden
+        set(value) {
+            view.hidden = value
+        }
+
+    /**
+     *  use it to set/get the [icon][Icon] of the imageButton
+     */
+    var icon: Icon
+        get() = view.icon
+        set(value) {
+            view.icon = value
+        }
+
+    /**
+     *  use it to set/get the onTap callback lambda of the imageButton
+     */
+    var onTap: (() -> Unit)?
+        get() =  view.onTap
+    set(value) {
+        view.onTap = value
+    }
 
 }

75f804c60503c5d4c4d9765bc38d6fb61fbddaa0
Mathieu Scotet <mscotet.lmit@gmail.com>
2023-03-06 10:39:45 +0100

    SKLoader : var onStateChanged:((visible:Boolean)->Unit)?

--- viewmodel/src/commonMain/kotlin/tech/skot/core/components/SKLoader.kt
+++ viewmodel/src/commonMain/kotlin/tech/skot/core/components/SKLoader.kt
@@ -14,11 +14,16 @@ class SKLoader : SKComponent<SKLoaderVC>() {
         loadingCounter--
     }
 
+    var onStateChanged:((visible:Boolean)->Unit)? = null
+
     fun isLoading():Boolean = loadingCounter > 0
     fun isNotLoading():Boolean = !isLoading()
 
     private var loadingCounter = 0
         set(value) {
+            if (field > 0 && value <= 0 || field <=0 && value > 0) {
+                onStateChanged?.invoke(value > 0)
+            }
             field = value
             view.visible = value > 0
         }

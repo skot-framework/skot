eae7a65e142328dae028382e6e86003c1cd1b80f
sgueniot <s.gueniot@useradgents.com>
2023-04-12 15:56:05 +0200

    update version name

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 
     const val group = "com.github.useradgents.skot"
-    const val version = "1.2.10-ua"
+    const val version = "1.2.11-ua"
 
     const val kotlin = "1.8.0"
     const val kotlinCoroutines = "1.6.4"
